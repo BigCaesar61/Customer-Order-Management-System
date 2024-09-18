a6048f4c26df2a3ceaaafdd66c385dac5f750758
Martin Leone <martinnikolaslll@gmail.com>
2024-09-18 12:03:38 -0400

    Create placeOrder function

--- Customer_Order_Management_System.js
+++ Customer_Order_Management_System.js
@@ -49,3 +49,28 @@ const inventory = [
     }
 
   ];
+
+
+  //Task 3: Create a Function to Place an Order
+
+  function placeOrder(customerName, orderedItems) {
+    let outOfStock = false;
+    orderedItems.forEach(item => {
+        const product = inventory.find(product => product.name === item.name);
+        if (!product) {
+            console.log(`Error: Product ${item.name} does not exist in the inventory.`);
+            outOfStock = true;
+        }
+    });
+    if (outOfStock) return; 
+    //above function checks to see if an item exists in our inventory
+    //if it does not it returns the error code and stops the order
+
+    orderedItems.forEach(item => {
+        const product = inventory.find(product => product.name === item.name);
+        product.quantity -= item.quantity
+    });
+    //this function processes an order by subtracting how much is ordered by the total amount
+    //of items
+
+  };
